7f46e39b9cb85e16aeba75efd1912063d49e6bab
fetisovad <fetis_94@mail.ru>
2021-03-11 23:11:30 +0500

    added parallax effect newslatter section

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
 });
-const reviewsSlider = new Swiper('.reviews-slider', {
+const reviewsSlider = new Swiper(".reviews-slider", {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
+});
+
+$(".parallax-window").parallax({
+  imageSrc: "img/newslatter-bg.jpg",
+  speed: "0.7",
 });
