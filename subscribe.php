00e560aa7d31d935921b805c2d8467ed5219219e
fetisovad <fetis_94@mail.ru>
2021-03-19 00:10:37 +0500

    added subscribe send

---
+++ subscribe.php
@@ -0,0 +1,51 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$subscribe = $_POST['subscribe'];
+// Формирование самого письма
+$title = "Новое сообщение Best Tour Plan - subscribe";
+$body = "
+<h2>Новое письмо</h2>
+<h3>У вас новая подписка на рассылку</h3>
+<b>email:</b><br>$subscribe";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'alexandrfetisov.web@gmail.com'; // Логин на почте
+    $mail->Password   = 'faYA@1994'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('alexandrfetisov.web@gmail.com', 'Александр Фетисов'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('fetis_94@mail.ru');  
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: send.html');
