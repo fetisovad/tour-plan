7fe58b34ff6d195b3c3a8dc522f80d3432b3d5ba
fetisovad <fetis_94@mail.ru>
2021-03-22 18:34:47 +0500

    added phone mask

--- js/main.js
+++ js/main.js
@@ -1,66 +1,70 @@
-const hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-$(".parallax-window").parallax({
-  imageSrc: "img/newslatter-bg.jpg",
-  speed: "0.7",
-});
-
-const mobileMenu = document.querySelector(".menu-button");
-mobileMenu.addEventListener("click", () => {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
-})
-
-const openModalBtnAll = Array.from(document.querySelectorAll('[data-modal]'))
-const openModalBtn = document.querySelector('[data-modal]')
-const closeModalBtn = document.querySelector('.modal__close')
-
-openModalBtnAll.forEach(item => {
-  item.addEventListener('click', openModal)
-})
-
-closeModalBtn.addEventListener("click", closeModal)
-document.addEventListener("keydown", closeModalEsc)
-
-function openModal() {
-  const modalWindow = document.querySelector('.modal__window');
-  const modalOverlay = document.querySelector('.modal__overlay');
-  modalWindow.classList.add('modal__window--visible')
-  modalOverlay.classList.add('modal__overlay--visible')
-}
-function closeModal(e) {
-  e.preventDefault();
-  const modalWindow = document.querySelector('.modal__window');
-  const modalOverlay = document.querySelector('.modal__overlay');
-  modalWindow.classList.remove('modal__window--visible')
-  modalOverlay.classList.remove('modal__overlay--visible')
-}
-function closeModalEsc(e) {
-  if (e.keyCode === 27) {
-    closeModal(e);
+$(document).ready(function(){
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  
+  $(".parallax-window").parallax({
+    imageSrc: "img/newslatter-bg.jpg",
+    speed: "0.7",
+  });
+  
+  const mobileMenu = document.querySelector(".menu-button");
+  mobileMenu.addEventListener("click", () => {
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+  })
+  
+  const openModalBtnAll = Array.from(document.querySelectorAll('[data-modal]'))
+  const openModalBtn = document.querySelector('[data-modal]')
+  const closeModalBtn = document.querySelector('.modal__close')
+  
+  openModalBtnAll.forEach(item => {
+    item.addEventListener('click', openModal)
+  })
+  
+  closeModalBtn.addEventListener("click", closeModal)
+  document.addEventListener("keydown", closeModalEsc)
+  
+  function openModal() {
+    const modalWindow = document.querySelector('.modal__window');
+    const modalOverlay = document.querySelector('.modal__overlay');
+    modalWindow.classList.add('modal__window--visible')
+    modalOverlay.classList.add('modal__overlay--visible')
   }
-}
+  function closeModal(e) {
+    e.preventDefault();
+    const modalWindow = document.querySelector('.modal__window');
+    const modalOverlay = document.querySelector('.modal__overlay');
+    modalWindow.classList.remove('modal__window--visible')
+    modalOverlay.classList.remove('modal__overlay--visible')
+  }
+  function closeModalEsc(e) {
+    if (e.keyCode === 27) {
+      closeModal(e);
+    }
+  }
+
+  $('.phone_us').mask('+7 (000) 000-00-00');
+});
